c8816790adcfe0cae67c34b70f5337a04f436b1b
chriskodehub <chris.kodehode@gmail.com>
2022-03-05 07:17:07 +0100

    feat: add js DataFrom() parsing to html form

--- js/index.js
+++ js/index.js
@@ -1,20 +1,63 @@
-// index.html
+const order = document.querySelector("form");
+const article = document.querySelector(".article-container");
 
-const btn = document.querySelector("#button");
-const sb = document.querySelector("#land");
-btn.onclick = (event) => {
+order.addEventListener("submit", parseForm);
+
+function parseForm(event) {
     event.preventDefault();
-    chooseFlag(sb.value);
-};
 
-function chooseFlag(land) {
-    if (land == "") {
-        showFlag("Select flag you want to display");
-    } else {
-        showFlag(`<img src="img/${land}.png" alt="flag of ${land}">`);
-    }
+    const form = new FormData(event.target);
+    const value = Object.fromEntries(form.entries());
+    article.innerHTML = printObject(value);
 }
 
-function showFlag(flag) {
-    document.getElementById("flag").innerHTML = flag;
+function printObject(object) {
+    let objectToString = "";
+    let orderInfo = [
+        ["client-type", "Client type"],
+        ["delivery-type", "Delivery type"],
+        ["name", "Name"],
+        ["sname", "Surname"],
+        ["phone", "Phone"],
+        ["email", "E-mail"],
+        ["street", "Street"],
+        ["postnr", "Postnr"],
+        ["town", "Town"],
+        ["login", "Login"],
+        ["pass", "Password"],
+        ["info", "Additional info"]];
+
+    orderInfo.forEach(element => {
+        objectToString += `<tr>
+            <td class="field-name">
+                ${element[1]}:
+            </td>
+            <td>
+                ${printSummary(element[0].toString(), object)}
+            </td>
+        </tr>`;
+    });
+
+    return `
+    <h1>Order summary</h1>
+    <div><table class="summary">
+        ${objectToString}
+    </table></div>
+    `;
+}
+
+function printSummary(element, object) {
+    if (element !== "info" && element !== "pass") {
+        return object[element];
+    }
+    else if (element === "pass") {
+        return "**********";
+    }
+    else if (element === "info") {
+        let specialField = object.info;
+        while (specialField.search(/\n/) !== -1) {
+            specialField = specialField.replace(/\n/, "<br>");
+        }
+        return specialField;
+    }
 }
